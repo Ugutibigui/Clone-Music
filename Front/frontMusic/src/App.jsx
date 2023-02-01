0fe7f4cb055c93515ecbf3b6b988ac24ffa68540
Ugutibigui <ugutibigui@gmail.com>
2023-02-01 18:08:19 -0300

    criando primeira estrutura front

--- Front/frontMusic/src/App.jsx
+++ Front/frontMusic/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
+import Card from './components/Card'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <div className="App">
       <div className="top">
-        <div className='left-menu'></div>
+        <div className='left-menu'>
+
+        </div>
         <div className='content'>
           <header>
 
