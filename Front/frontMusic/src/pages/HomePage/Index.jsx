1fba5f6c83f5a367018a1ad65b95639bae4cb0f2
Richard Luan <richardluan084@gmail.com>
2023-02-01 22:29:30 -0300

    Criando header

--- Front/frontMusic/src/pages/HomePage/Index.jsx
+++ Front/frontMusic/src/pages/HomePage/Index.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import './Index.css'
+
 import deezer from '../../assets/deezerWhite.png'
+import iconMusic from '../../assets/icons/music.svg'
+import iconMic from '../../assets/icons/mic.svg'
+import iconRadio from '../../assets/icons/radio.svg'
+import iconExplore from '../../assets/icons/explore.svg'
+import iconFavorite from '../../assets/icons/favorite.svg'
+import iconSearch from '../../assets/icons/search.svg'
+import iconClose from '../../assets/icons/close.svg'
+
+import CardOption from '../../components/CardOption'
 
 
 function Index() {
+
+	const icons = [iconMusic, iconMic, iconRadio, iconExplore, iconFavorite]
 
 	return (
 		<div className="App">
@@ -21,12 +33,22 @@ function Index() {
 					</div>
 
 					<div className="options">
-						
+						<div className="options-ref">
+							<ul>
+								<CardOption/>
+							</ul>
+						</div>
 					</div>
 				</div>
 				<div className='content'>
 					<header>
-
+						<div className="text-box">
+							<div className="search">
+								<button>{iconSearch}</button>
+								<input type="text" placeholder='Buscar'/>
+								<button>{iconClose}</button>
+							</div>
+						</div>
 					</header>
 					<div className='info-content'>
 
