bbee316439e79b9581586063cce9141a918a056c
Richard Luan <richardluan084@gmail.com>
2023-02-02 19:22:40 -0300

    Estilizando header e usando react-icons

--- Front/frontMusic/src/pages/HomePage/Index.jsx
+++ Front/frontMusic/src/pages/HomePage/Index.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 import './Index.css'
-
 import deezer from '../../assets/deezerWhite.png'
-import iconMusic from '../../assets/icons/music.svg'
-import iconMic from '../../assets/icons/mic.svg'
-import iconRadio from '../../assets/icons/radio.svg'
-import iconExplore from '../../assets/icons/explore.svg'
-import iconFavorite from '../../assets/icons/favorite.svg'
-import iconSearch from '../../assets/icons/search.svg'
-import iconClose from '../../assets/icons/close.svg'
+
+import {FaMusic} from 'react-icons/fa'
+import {FaMicrophoneAlt} from 'react-icons/fa'
+import {MdOutlineRadio} from 'react-icons/md'
+import {GrAppsRounded} from 'react-icons/gr'
+import {MdFavoriteBorder} from 'react-icons/md'
+import {FiSearch} from 'react-icons/fi'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {MdNotifications} from 'react-icons/md'
+import {MdAccountCircle} from 'react-icons/md'
 
 import CardOption from '../../components/CardOption'
 
 
 function Index() {
 
-	const icons = [iconMusic, iconMic, iconRadio, iconExplore, iconFavorite]
+	const icons = [<FaMusic/>, <FaMicrophoneAlt/>, <MdOutlineRadio/>, <GrAppsRounded/>, <MdFavoriteBorder/>]
 
 	return (
 		<div className="App">
@@ -44,9 +46,16 @@ function Index() {
 					<header>
 						<div className="text-box">
 							<div className="search">
-								<button>{iconSearch}</button>
+								<button> <FiSearch size={20}/> </button>
 								<input type="text" placeholder='Buscar'/>
-								<button>{iconClose}</button>
+							</div>
+							<button> <AiFillCloseCircle size={35}/> </button>
+						</div>
+
+						<div className="profile">
+							<div className="icons-profile">
+								<button> <MdNotifications size={30}/> </button>
+								<button> <MdAccountCircle size={30}/> </button>
 							</div>
 						</div>
 					</header>
