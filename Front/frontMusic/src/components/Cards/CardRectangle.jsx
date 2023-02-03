6c2a5d33622ba245f2f7822d493a8fe363bc297f
Ugutibigui <ugutibigui@gmail.com>
2023-02-03 17:09:07 -0300

    props color para card

--- Front/frontMusic/src/components/Cards/CardRectangle.jsx
+++ Front/frontMusic/src/components/Cards/CardRectangle.jsx
@@ -2,7 +2,7 @@ import '../Content/Musica/Music'
 
 function CardRectangle(props) {
 	return (
-		<div className="rectangle" style={{background}}>
+		<div className="rectangle" style={{backgroundImage: `linear-gradient(to right, ${props.colorOne} , ${props.colorTwo})`}}>
             <img src="https://e-cdns-images.dzcdn.net/images/misc/ed75a34e8f911b6bd636144096678a9c/142x142-none-80-0-0.png" alt="??" />
 
             {props.title ? <p>{props.title}</p> : <p></p> }
