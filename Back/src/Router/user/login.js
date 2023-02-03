e1a96292813058dd063faa1fc19993ba47a37b27
Ugutibigui <ugutibigui@gmail.com>
2023-02-03 16:59:29 -0300

    criando sistema de login no backend

--- Back/src/Router/user/login.js
+++ Back/src/Router/user/login.js
@@ -1,17 +1,60 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 const db = require("../../db/db")
 
 router.post('/', async (req, res) => {
-    let info = req.body;
-    console.log(info)
-     res.send("teste")
-    try {
-      console.log("Teste")
-    } catch (err) {
+      console.log("foi")
+      let info = req.body;
+      const email = info.email
+      const password = info.password
 
-    }
+      var param = {
+            email: email,
+            password: password
+      }
 
+      var response = {
+            ok: false,
+            message: ""
+      }
+
+      console.log(param)
+
+      try {
+            if (!email || !password) {
+                  return { erro: 'Dados insuficientes.' }
+            }
+
+            const [login] = await db.searchInfosLogin(param);
+            console.log(login.length)
+
+            if (login.length > 0) {
+
+                  console.log(login)
+                  
+                   let Token = await jwt.sign({
+                        id: login.id,
+                        name: login.name,
+                        email: login.email
+                  }, 'passwordForToken');
+
+                  //salva o token nos cookies
+
+                  response.ok = true
+                  response.message = 'logged'
+                  res.cookie('Token', Token);
+                  res.sendStatus(200);
+            } else {
+                  response.ok = false
+                  response.message = 'E-mail ou senha incorretos'
+                  res.json(response)
+            }
+      } catch (err) {
+            console.log(err)
+            res.send("Ocorreu um erro interno")
+      }
+      
 })
 
 module.exports = router;
--- Back/src/index.js
+++ Back/src/index.js
@@ -1,15 +1,28 @@
 const express = require("express")
-const cors = require("cors")
+const cors = require('cors')
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 const app = express();
 const port = 8000
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
 
 const login = require("./Router/user/login")
 app.use("/user/login", login)
+
+const logado = require("./Router/user/logado")
+app.use("user/verifyLogin", logado)
+
+const logoff = require("./Router/user/logoff")
+app.use("user/logoff", logoff)
+
+
+app.use("/", (req, res) => {
+      res.send("bom dia")
+})
 
 app.listen(port, () => {
       console.log("Servidor Ligadoo")
