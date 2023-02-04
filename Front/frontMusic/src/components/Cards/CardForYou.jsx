d2d167e03597b93d693472d0c3ad58ba38202891
Richard Luan <richardluan084@gmail.com>
2023-02-04 19:02:38 -0300

    Estilização do componente do for you finalizada

--- Front/frontMusic/src/components/Cards/CardForYou.jsx
+++ Front/frontMusic/src/components/Cards/CardForYou.jsx
@@ -1,30 +1,31 @@
 import '../Content/Musica/Music'
-import {ImPlay3} from 'react-icons/im'
-import {FaDeezer} from 'react-icons/fa'
+import { ImPlay3 } from 'react-icons/im'
+import { FaDeezer } from 'react-icons/fa'
 
 
 function CardForYou() {
-	return (
-		<div className="image-singer">
+    return (
+        <div className="image-singer">
             <div className="image">
-                <img src=""/>
+                <img src="https://e-cdns-images.dzcdn.net/images/artist/75317cac54ca471123785d7c701e815d-a9cb605a0b64f652ec004187aba94ad8-18b811f5b9bac7241958b29682cae546-4b5733b0e49a754c4c776c9228990972/264x264-000000-80-0-0.jpg" />
 
                 <div className="logo-daily">
-                    <FaDeezer/> Daily
+                    <div className="svg-icon">
+                        <FaDeezer /> Daily
+                    </div>
                 </div>
 
                 <div className="play-button">
                     <button>
-                        <ImPlay3/>
+                        <ImPlay3 />
                     </button>
                 </div>
             </div>
 
-            <div className="text">
-                <p>Destacando Munhoz & Mariano, Gusttavo Lima, Jorge & Mateus, Henrique & Juliano</p>
-            </div>
+            <p>Destacando Munhoz & Mariano, Gusttavo Lima, Jorge & Mateus, Henrique & Juliano</p>
+
         </div>
-	)
+    )
 }
 
 export default CardForYou
