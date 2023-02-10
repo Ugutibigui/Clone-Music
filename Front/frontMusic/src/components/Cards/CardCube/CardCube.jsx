eafea41ae9c8708b995a775b35faf30190964d90
Richard Luan <richardluan084@gmail.com>
2023-02-10 14:59:09 -0300

    Criando botões de slide

--- Front/frontMusic/src/components/Cards/CardCube/CardCube.jsx
+++ Front/frontMusic/src/components/Cards/CardCube/CardCube.jsx
@@ -1,9 +1,18 @@
 import './CardCube.css'
+import { ImPlay3 } from 'react-icons/im'
 
 function CardCube(props) {
     return (
         <div className="image-cube">
-            <img src='https://e-cdns-images.dzcdn.net/images/playlist/6ebfed888b3b116667d2b2897c995f29/264x264-000000-80-0-0.jpg' alt="Artista" />
+            <div className="image-end">
+                <img src='https://e-cdns-images.dzcdn.net/images/playlist/6ebfed888b3b116667d2b2897c995f29/264x264-000000-80-0-0.jpg' alt="Artista" />
+
+                <div className="play-button-end">
+                    <button>
+                        <ImPlay3 />
+                    </button>
+                </div>
+            </div>
 
             <span>Top 50 Sertanejo</span>
 
--- Front/frontMusic/src/components/Section/Channel/Channel.jsx
+++ Front/frontMusic/src/components/Section/Channel/Channel.jsx
@@ -1,4 +1,6 @@
 import './Channel.css'
+import { AiOutlineLeft } from 'react-icons/ai'
+import { AiOutlineRight } from 'react-icons/ai'
 
 const h3 = (props) => {
     if (props.h3) {
@@ -6,20 +8,30 @@ const h3 = (props) => {
     }
 }
 
-function CardFavorite(props) {
+function Channel(props) {
     return (
         <section className='channel'>
             <div className="container">
-                <h2>{props.h2}</h2>
-                
-                {h3(props)}
+                <div className="headers">
+                    <div className="texts">
+                        <h2>{props.h2}</h2>
+
+                        {h3(props)}
+                    </div>
+
+                    <div className="sliders">
+                        <button id='button-left'><AiOutlineLeft /></button>
+                        <button id='button-right'><AiOutlineRight /></button>
+                    </div>
+                </div>
 
                 <div className="organize">
                     <div className="gallery">
-                        <props.card image='' title='' fans='' date='' author='' tracks=''/>
-                        <props.card image='' title='' fans='' date='' author='' tracks=''/>
-                        <props.card image='' title='' fans='' date='' author='' tracks=''/>
-                        <props.card image='' title='' fans='' date='' author='' tracks=''/>
+                        <props.card image='' title='' fans='' date='' author='' tracks='' />
+                        <props.card image='' title='' fans='' date='' author='' tracks='' />
+                        <props.card image='' title='' fans='' date='' author='' tracks='' />
+                        <props.card image='' title='' fans='' date='' author='' tracks='' />
+                        <props.card image='' title='' fans='' date='' author='' tracks='' />
                     </div>
                 </div>
             </div>
@@ -27,4 +39,4 @@ function CardFavorite(props) {
     )
 }
 
-export default CardFavorite
+export default Channel
