3cf2ba8d4c6c58383bccca5e9714925a52c02ed3
Richard Luan <richardluan084@gmail.com>
2023-02-10 17:53:59 -0300

    Finalizando Card Favorite

--- Front/frontMusic/src/components/Cards/CardFavorite/CardFavorite.jsx
+++ Front/frontMusic/src/components/Cards/CardFavorite/CardFavorite.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import './CardFavorite.css'
+import { ImPlay3 } from 'react-icons/im'
+import { FaHeart } from 'react-icons/fa'
 
 function CardFavorite(props) {
+
+    const [className, setClassName] = useState('btn');
+
+    const handleMouseEnter = () => {
+        setClassName('add-button');
+    };
+
+    const handleMouseLeave = () => {
+        setClassName('btn');
+    };
+
     return (
-        <div className="image-round"> 
-            <img src='https://e-cdns-images.dzcdn.net/images/artist/a9cb605a0b64f652ec004187aba94ad8/264x264-000000-80-0-0.jpg' alt="Artista" /> 
+        <div className="image-round">
+            <div className="image-center">
+                <img 
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave} 
+                src='https://e-cdns-images.dzcdn.net/images/artist/a9cb605a0b64f652ec004187aba94ad8/264x264-000000-80-0-0.jpg' alt="Artista" />
+
+                <div className="buttons-round">
+                    <button className={className}>
+                        <ImPlay3 />
+                    </button>
+
+                    <button className={className}>
+                        <FaHeart id='hearth'/>
+                    </button>
+                </div>
+            </div>
 
             <span>Gusttavo Lima</span>
 
