8ab578dc9b45da2f664b7bb89f0f9da2b19b238e
Richard Luan <richardluan084@gmail.com>
2023-02-10 18:23:11 -0300

    Finalizando card radio

--- Front/frontMusic/src/components/Cards/CardRadio/CardRadio.jsx
+++ Front/frontMusic/src/components/Cards/CardRadio/CardRadio.jsx
@@ -2,23 +2,23 @@ import './CardRadio.css'
 import { ImPlay3 } from 'react-icons/im'
 
 function CardRadio(props) {
-	return (
-		<div className='cardRadio'>
-			<div className="radio-image">
+    return (
+        <div className='cardRadio'>
+            <div className="radio-image">
                 <img src="https://e-cdns-images.dzcdn.net/images/misc/3d25d3524c0628bb23e769c386542b87/264x264-000000-80-0-0.jpg" alt="Radio" />
+            </div>
 
-                <div className="play-button-end">
-                    <button>
-                        <ImPlay3/>
-                    </button>
-                </div>
+            <div className="play-button-radio">
+                <button>
+                    <ImPlay3 />
+                </button>
             </div>
 
             <span>Ao vivo</span>
 
             <div id='channel'>Antena 1</div>
-		</div>
-	)
+        </div>
+    )
 }
 
 export default CardRadio
