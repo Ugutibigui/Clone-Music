a36bb8ee72fbec76de7160b405e604a1d632e9ef
= <richardluan084@gmail.com>
2023-05-12 18:11:16 -0300

    Atualizações + API

--- Front/frontMusic/src/pages/Content/Musica/Music.jsx
+++ Front/frontMusic/src/pages/Content/Musica/Music.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import Channel from '../../../components/Layout/Channel/Channel'
 import Button from '../../../components/Buttons/Button/Button';
@@ -15,32 +16,31 @@ import styles from './Music.module.css'
 
 function Music() {
 
+    const [artist, setArtist] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8000/artists', {
+            'method': 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json()) // Randomizando e mostrando somente 5 artistas
+            .then(data => setArtist(data.sort(() => Math.random() - 0.5).slice(0, 5))) 
+    }, [])
+
     return (
         <div className={styles.infoContent}>
             <section className={styles.channel}>
                 <div className={styles.artistAdd}>
                     <p>Desbloqueie seu Flow, selecionando seus artistas favoritos para obter recomendações melhores.</p>
 
-                    <div className={styles.artists} /* Temporario */>
-                        <div className={styles.figureImage}>
-                            <img src='https://e-cdns-images.dzcdn.net/images/artist/6f797dbd305547a2b793858497ed0ecd/162x162-000000-80-0-0.jpg' id={`img${0}`} />
-                        </div>
-
-                        <div className={styles.figureImage}>
-                            <img src='https://e-cdns-images.dzcdn.net/images/artist/62e6edfaf5461eeb5b7310903229607a/264x264-000000-80-0-0.jpg' id={`img${1}`} />
-                        </div>
-
-                        <div className={styles.figureImage}>
-                            <img src='https://e-cdns-images.dzcdn.net/images/artist/ba30ff888a06c1253f9138f7a49e0ff2/264x264-000000-80-0-0.jpg' id={`img${2}`} />
-                        </div>
-
-                        <div className={styles.figureImage}>
-                            <img src='https://e-cdns-images.dzcdn.net/images/artist/7260526934f80aab3f3c37221b9132ca/264x264-000000-80-0-0.jpg' id={`img${3}`} />
-                        </div>
-
-                        <div className={styles.figureImage}>
-                            <img src='https://e-cdns-images.dzcdn.net/images/artist/a143873fc7a5984edc629eadd5856861/264x264-000000-80-0-0.jpg' id={`img${4}`} />
-                        </div>
+                    <div className={styles.artists}>
+                        {artist.map((item, index) => (
+                            <div className={styles.figureImage}>
+                                <img src={item.image} alt="Artist" id={`img${index}`} />
+                            </div>
+                        ))}
                     </div>
 
                     <Link to='/recommendation'>
